09dfcf72605db19869804c06f07f7092d151afc1
Admin <admin@127.0.0.1>
2024-06-29 21:43:40 -0700

    Adding in Edges and Perks

--- app/components/wod5e-sheet.js
+++ app/components/wod5e-sheet.js
@@ -4,9 +4,11 @@ export default class WoD5eSheet extends Component {
 
     shouldShow = (data) => !!data;
 
-    parse = (data) => JSON.parse(data)
+    parse = (data) => JSON.parse(data);
 
-    keys = (obj) => Object.keys(obj)
+    getType = (data) => JSON.parse(data).type;
+
+    getPowerHeader = (data) => JSON.parse(data).powers_title;
 
     formatAttribs = (data) => this.column_format(this.parse(data).attribs, 3, (k, v) => ({ name: k, value: v }))
 
@@ -17,6 +19,32 @@ export default class WoD5eSheet extends Component {
         format_value: obj.value + obj.secondary_value ? ` ${obj.secondary_value}` : '',
         children: Object.values(obj.children).map((inner) => ({ ...inner, name: `  -${inner.name}` }))
     }))
+
+    formatEdges = (data) => {
+        let sheet = this.parse(data);
+        let edges = []
+
+        Object.entries(sheet.edges).forEach((e) => edges = edges.concat([e[0], ...e[1].map((p) => `&nbsp;&nbsp;-${p}`)]));
+
+        let midpoint = Math.floor(edges.length / 2) + (edges.length % 2)
+
+        // We want to keep sub-objects in the same column as their parent.
+        // If the first item in the second column is a sub-object (starts with a space),
+        // push forward until we find a main-line item.
+
+        while (midpoint > 0 && midpoint < edges.length && edges[midpoint].startsWith('&nbsp;')) {
+            midpoint++;
+        }
+
+        let values = []
+
+        for (let row = 0; row < midpoint; row++) {
+            values.push(edges[row]);
+            values.push(edges[row + midpoint] ?? '');
+        }
+
+        return values;
+    }
 
     column_format = (items, columnCount, format) => {
         let val = []
